faaf41e0ef7981e7ac62cabe2a999844948e60fd
Dmitriy Yakovets <dmitriy.yakovets@gmail.com>
2023-10-17 00:11:42 +0300

    feat: Настройка проекта, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@fontsource-variable/montserrat';
+import { Search } from 'lucide-react';
+import { ReactElement } from 'react';
+import styles from './App.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = (): ReactElement => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <h1 className={styles['header-text']}>Search for books</h1>
+        <div className={styles['search-field-wrapper']}>
+          <input type="search" className={styles['search-field']} />
+          <Search size={24} color="#000" />
+        </div>
+        <div className={styles['filter-container']}>
+          <div className={styles['filter-select-wrapper']}>
+            <span className={styles['filter-select-name']}>Categories</span>
+            <select className={styles['filter-select-field']}>
+              <option className={styles['filter-select-value']}>all</option>
+            </select>
+          </div>
+          <div className={styles['filter-select-wrapper']}>
+            <span className={styles['filter-select-name']}>Sorting by</span>
+            <select className={styles['filter-select-field']}>
+              <option className={styles['filter-select-value']}>
+                relevance
+              </option>
+            </select>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    </main>
+  );
+};
